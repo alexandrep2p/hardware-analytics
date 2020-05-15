3ba677628b4beb1dc90e30db932c58c924a06511
Alexandre Abreu <alexandre_1605@outlook.com>
2020-05-14 22:45:09 -0300

    Preços min-max no periodo, desvio padrao no periodo, alterção de layout, correção bug do chart ao fazer update de dataset

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,15 +10,19 @@ import * as Chart from 'chart.js';
 })
 export class AppComponent implements OnInit {
 
+  @ViewChild('productChart') productChart: ElementRef;
+
   productPrices: Array<number> = [];
   productDates: Array<any> = [];
   sellers: string[] = [];
   products: any[] = [];
   selectedSeller = 'Vendedor';
-  selectedProduct = 'Selecione um vendedor';
+  selectedProduct = 'Selecione um produto';
+  maxPrice;
+  minPrice;
   actualPrice;
-
-  @ViewChild('productChart') productChart: ElementRef;
+  chart: Chart;
+  deviation: number;
 
   constructor(
     private productService: ProductService
@@ -29,60 +33,74 @@ export class AppComponent implements OnInit {
     this.getSellers();
   }
 
-  getSellers(){
+  getSellers() {
     this.productService.getSellers()
-    .subscribe(result => {
-      result.forEach(seller => {
-        this.sellers.push(seller.DISTINCT);
+      .subscribe(result => {
+        result.forEach(seller => {
+          this.sellers.push(seller.DISTINCT);
+        });
       });
-    });
   }
 
-  selectSeller(seller: string){
+  selectSeller(seller: string) {
     this.selectedSeller = seller;
     this.products = [];
     this.getProductsFromSeller(seller);
   }
 
-  getProductsFromSeller(seller: string){
+  getProductsFromSeller(seller: string) {
     this.productService.getProductsFromSeller(seller)
-    .subscribe(product => {
-      this.products = product;
-    });
+      .subscribe(product => {
+        this.products = product;
+      });
   }
 
-  selectProduct(product: string){
+  selectProduct(product: string) {
     this.selectedProduct = product;
+    this.productPrices = [];
+    this.productDates = [];
     this.getProduct(this.selectedProduct, this.selectedSeller);
   }
 
-  getProduct(name: string, seller: string){
-    this.productPrices = [];
-    this.productDates = [];
-    this.productService.getProductData(name , seller)
-    .subscribe( product => {
-      product.forEach(item => {
-        this.productPrices.push(item.price);
-        this.productDates.push(moment(item.createdAt).format('DD/MM/YYYY'));
+  getProduct(name: string, seller: string) {
+    this.productService.getProductData(name, seller)
+      .subscribe(product => {
+        product.forEach(item => {
+          this.productPrices.push(item.price);
+          this.productDates.push(moment(item.createdAt).format('DD/MM/YYYY'));
+        });
+        this.actualPrice = this.productPrices.slice(-1)[0];
+        this.minPrice = Math.min.apply(Math, this.productPrices);
+        this.maxPrice = Math.max.apply(Math, this.productPrices);
+        const average = this.productPrices.reduce((total, value) =>
+          total + value / this.productPrices.length, 0);
+        const variance = this.productPrices.reduce((total, value) =>
+          total + Math.pow(average - value, 2) / this.productPrices.length, 0);
+        this.deviation = Math.sqrt(variance);
+
+        this.plotChart(this.productPrices, this.productDates, this.selectedProduct);
       });
-      this.actualPrice = this.productPrices.slice(-1)[0];
-      this.plotChart(this.productPrices, this.productDates, this.selectedProduct);
+  }
+
+  plotChart(prices: Array<number>, dates: Array<any>, productName) {
+    if (this.chart){
+      this.destroyChart(this.chart);
+    }
+    this.chart = new Chart(this.productChart.nativeElement, {
+      type: 'line',
+      data: {
+        labels: dates,
+        datasets: [{
+          label: productName,
+          fill: false,
+          borderColor: 'rgb(242,112,19)',
+          data: prices
+        }]
+      }
     });
   }
 
-  plotChart(prices: Array<number>, dates: Array<any>, productName){
-    const chart = new Chart(this.productChart.nativeElement, {
-        type: 'line',
-        data: {
-            labels: dates,
-            datasets: [{
-                label: productName,
-                backgroundColor: 'rgb(255, 99, 132)',
-                borderColor: 'rgb(255, 99, 132)',
-                data: prices
-            }]
-        }
-    });
-    chart.update();
+  destroyChart(chart: Chart) {
+    chart.destroy();
   }
 }
